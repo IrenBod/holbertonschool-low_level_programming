812cda6b6f3415300166d2ec5040088cd05bc8b8
IrenBod <>
2024-06-17 11:28:53 +0000

    Task 4 Hello, puts C - Hello, World

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	char str[55]="Programming is like building a multilingual puzzle";
+	char str[55]="\"Programming is like building a multilingual puzzle";
 	puts(str);
 
 	return 0;
