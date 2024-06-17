692d9702df0876ac0c4d58217bff29d87b38deba
IrenBod <>
2024-06-17 13:00:35 +0000

    Le task 5. Hello, printf C - Hello, World

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
+/**
+* main - Printf "with proper grammar, but the outcome is a piece of art,
+*
+* Return: Always 0.
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	return 0;
-
+	return (0);
 }
