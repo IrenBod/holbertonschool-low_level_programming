d86b13c7583cd0f7e2a762b3351486dcbc788d7d
IrenBod <>
2024-06-17 11:45:10 +0000

    Task 4 Hello, puts C - Hello, World

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 int main(void)
-	/* Le premiere programm C */
+	/* Le premiere
+	 * programm C 
+	 * a Holberton*/
 {
 	char str[55] = "\"Programming is like building a multilingual puzzle";
 
