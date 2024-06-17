45c457021cccad7c19fbb8aaf50b067599872f1c
IrenBod <>
2024-06-17 12:08:19 +0000

    Task 4 Hello, puts C - Hello, World

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 int main(void)
-	/*
-	 Le premiere programm C a Holberton 
-	 fonction pour afficher la ligne des mots
-	 Programming is like building a multilingual puzzle
-	 */
+
+/*
+*Main fonction  
+*Programming is like building a multilingual puzzle
+*/
+
 {
 	char str[55] = "\"Programming is like building a multilingual puzzle";
 
