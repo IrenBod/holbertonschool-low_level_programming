5600f45bd192538007f6a457d358024448a3e678
IrenBod <>
2024-06-18 13:55:10 +0000

    Task 3 alphABET C - Variables, if, else, while

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+int i;
+
+for (i = 97; i <= 122; i++)
+{
+putchar(i);
+}
+for (i = 65; i <= 90; i++)
+{
+putchar(i);
+}
+putchar('\n');
+
+return (0);
+}
