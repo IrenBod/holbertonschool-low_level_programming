803424afe7583ccb8e46b1eb050bf97cadca3562
IrenBod <>
2024-06-18 08:26:47 +0000

    Task 0 de C - Variables, if, else, while

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+* main - Printf "Positive anything is better than negative nothing
+*
+* Return: Always 0.
+*/
+
 int main(void)
 {
 	int n;
