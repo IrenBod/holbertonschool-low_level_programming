60d58c67dc4da02cfc19f886a21abf58e96c95a8
IrenBod <>
2024-06-19 17:01:09 +0000

    Le task 2 10 x alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*2-print_alphabet_x10.c - print: a function that prints 10 times the alphabet
+*2-print_alphabet_x10.c-print: a function that prints 10 times the alphabet
 *Return: 0
 */
 
@@ -10,9 +10,14 @@ void print_alphabet_x10(void)
 int x;
 int i;
 	for (x = 0; x < 10; x++)
+	{
 		for (i = 97; i <= 122; i++)
 		{
-			_putchar('\n');
 			_putchar(i);
 		}
+		
+		_putchar('\n');
+	
+	}
+
 }
