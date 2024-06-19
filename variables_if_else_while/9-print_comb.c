0caa0c7bdc5ecf7aa4849fa5a1ccfdf173e429e6
IrenBod <>
2024-06-19 07:22:22 +0000

    Le task 9 Patience, persistence and perspiration. C - Variables, if, else, while

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+*main-print: Patience, persistence and perspiration make
+*an unbeatable combination for success
+*Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+{
+	putchar(i);
+
+	if (i == 57)
+		break;
+	putchar(44);
+	putchar(32);
+}
+	putchar('\n');
+
+	return (0);
+}
