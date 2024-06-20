8cb0c807ec140cf5a5f46d9ead95ee640cd33b98
IrenBod <>
2024-06-20 12:00:28 +0000

    Le task 5  Sign C - Functions, nested loops

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+*print_sign - function that prints the sign of a number
+*@n: int
+*Return: 0
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+	_putchar('0');
+	}
+	else
+	{
+		return (-1);
+	_putchar('-');
+	}
+
+}
