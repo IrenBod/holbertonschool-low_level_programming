9e26c4f97249c70d24a514d08b7bc99e749db2af
IrenBod <>
2024-06-21 11:49:55 +0000

    Le task 10 a+b C - Functions, nested loops

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*main
+*add-print:a function that adds two integers and returns the result.
+@parameter: a int, b int, add int
+*/
+
+int add(int a, int b)
+{
+
+	return (a + b);
+	
+}
---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*times_table-print: a function that prints the 9 times table, starting with 0
+@: void
+*/
+
+void times_table(void)
+{
+
+	int car = 2;
+	int v = 2;
+
+	_putchar(car*v);
+
+}
+
+
+
