bf11939bf4fbcf2adb4a3a6d27910569f046bb91
IrenBod <>
2024-06-25 15:50:35 +0000

    3. I do not fear computers. I fear the lack of them

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _puts - a function that prints a string, followed by a new line
+*@str: char
+*Return: 0
+*/
+
+void _puts(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\n'; i++)
+	_putchar(str[i]);
+}
