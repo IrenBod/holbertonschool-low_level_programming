1ee08d93da91abbe1321419e888140f0c7eec340
IrenBod <>
2024-06-26 20:35:46 +0000

    la preparation des tasks pour le code

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _isupper - Write a function that checks for uppercase character.
+* @c: int
+*Return: 0
+*/
+
+void print_array(int *a, int n)
+
+{
+
+}
---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _isupper - Write a function that checks for uppercase character.
+* @c: int
+*Return: 0
+*/
+
+char *_strcpy(char *dest, char *src)
+
+{
+
+}
