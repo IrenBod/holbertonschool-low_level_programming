0084bbdfbf5e9750e411cc4de84dc3a9c6d6e903
IrenBod <>
2024-06-26 20:15:51 +0000

    7. Winning is only half of it. Having fun is the other half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,13 +1,35 @@
 #include "main.h"
 
 /**
-* _isupper - Write a function that checks for uppercase character.
-* @c: int
+* puts_half - a function that prints half of a string, followed by a new line.
+* @str: char
 *Return: 0
 */
 
 void puts_half(char *str)
 
 {
+	int i;
+	int start;
+	int size;
 
+	size = 0;
+
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+	if (size % 2 == 0)
+	{
+		start = size / 2;
+	}
+	else
+	{
+		start = (size - 1) / 2;
+	}
+	for (i = start; i < size; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
