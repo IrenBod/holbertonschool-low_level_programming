20db2357439fa7a78e5062960950a5b64c1bb610
IrenBod <>
2024-06-29 14:32:12 +0000

    3. strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,16 +9,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-    while (*s1 == *s2)
-    {
-        if (*s1 == '\0')
-            return 0;
-        s1++;
-        s2++;
-    }
-
-    if (*s1 < *s2)
-        return -13;
-    else
-        return 13;
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		return (0);
+	s1++;
+	s2++;
+	}
+	if (*s1 < *s2)
+	return (-13);
+	else
+	return (13);
 }
