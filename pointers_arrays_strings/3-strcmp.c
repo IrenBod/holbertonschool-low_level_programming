30594161e2040344bad2281e1196953173be246f
IrenBod <>
2024-06-29 13:06:52 +0000

    3. strcmp C - More pointers, arrays and strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,16 +9,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1  == *s2)
-	{
-		if (*s1 == '\0')
-			return (0);
-		s1++;
-		s2++;
-	}
+    while (*s1 == *s2)
+    {
+        if (*s1 == '\0')
+            return 0;
+        s1++;
+        s2++;
+    }
 
-	if (*s1 < *s2)
-		return (-1);
-	else
-		return (1);
+    if (*s1 < *s2)
+        return -1;
+    else
+        return 1;
 }
