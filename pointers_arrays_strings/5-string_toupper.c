3535e8cb9ef8c8de261bdb24b23c7e3bc1e80b6e
IrenBod <>
2024-06-30 08:01:00 +0000

    5. Always look up

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -12,7 +12,7 @@ char *string_toupper(char *str)
 int i = 0;
 
 	while (str[i] != '\0')
-	if (i >= 97 && i <= 122)
+	if (str[i] >= 'a' && str[i] <= 'z')
 	str[i] = str[i] - ('a' - 'A');
 		i++;
 		return (str);
