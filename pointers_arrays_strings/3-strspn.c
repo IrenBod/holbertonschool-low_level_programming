996db15bf3e09401af38aaccd0958e76100a3997
IrenBod <>
2024-07-01 18:16:00 +0000

    3. strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,36 +1,37 @@
 #include "main.h"
 
 /**
-* _isupper - Write a function that checks for uppercase character.
-* @c: int
+* _strspn - a function that gets the length of a prefix substring.
+* @s: char
+* @accept: char
 *Return: 0
 */
 
 unsigned int _strspn(char *s, char *accept)
 
 {
-	unsigned int count = 0;
+unsigned int count = 0;
 
-	while (*s != '\0')
+while (*s != '\0')
+{
+	char *a = accept;
+	int found = 0;
+
+	while (*a != '\0')
 	{
-	 char *a = accept;
-	 int found = 0;
-	 
-	 while (*a != '\0')
-	 {
-		 if(*s == *a)
-		 {
-		 found = 1;
-		 break;
-		 }
-		 a++;
-	 }
-	 if (!found)
-	 {
-		 break;
-	 }
-	 count++;
-	 s++;
+		if (*s == *a)
+		{
+			found = 1;
+			break;
+		}
+		a++;
 	}
-	return  count;
+	if (!found)
+	{
+		break;
+	}
+	count++;
+	s++;
+}
+	return (count);
 }
