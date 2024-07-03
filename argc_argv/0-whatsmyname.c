b68df2c103950f4ba7e826370aec961421c79577
IrenBod <>
2024-07-03 15:18:02 +0000

    0. It aint what they call you, its what you answer to C - argc, argv

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,14 +2,15 @@
 #include<stdio.h>
 
 /**
-* whatsmyname - a program that prints its name, followed by a new line
+* main - a program that prints its name, followed by a new line
 * @argv: char
+* @argc: int
 *Return: 0
 */
 
 int  main(int argc, char *argv[])
 
-{	
+{
 	(void)argc;
 	printf("%s\n", argv[0]);
 	return (0);
