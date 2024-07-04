0576ca2a5e9b4a9d7326b6fc97b563eb6e0ba7c9
IrenBod <>
2024-07-04 07:36:06 +0000

    Silence is argument carried out by other means

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strlen_recursion - a function that returns the length of a string
+* @s: char
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)*argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}	
