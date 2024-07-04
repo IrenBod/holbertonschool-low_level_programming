fe1349815cf8a6c131b54c94801707ab29633b87
IrenBod <>
2024-07-04 18:05:45 +0000

    4. To infinity and beyond

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -13,15 +13,24 @@ int main(int argc, char *argv[])
 
 {
 	int i;
+	int j;
 	int sum = 0;
 
-	if (argc < 3)
+	if (argc == 1)
 	{
 		printf("0\n");
-		return (1);
+		return (0);
 	}
 	for (i = 1; i < argc; i++)
 	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!(argv[i][j] >= 48 && argv[i][j] <= 9))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
