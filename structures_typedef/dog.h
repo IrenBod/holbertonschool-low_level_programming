c97b1d23b3b9931619c528f20c475ecf427e84f0
IrenBod <>
2024-07-12 19:52:34 +0000

    Projet P2 C - Structures, typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -11,7 +11,6 @@
  *
  * Description: Longer description
  */
-char *_strdup(char *str);
 
 struct dog
 {
@@ -26,4 +25,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strdup(char *str);
 #endif
