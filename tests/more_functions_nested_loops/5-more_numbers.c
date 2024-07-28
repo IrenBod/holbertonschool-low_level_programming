a4de5440410f3bdfe92eddbb4d112cad126b8517
IrenBod <>
2024-07-28 20:25:48 +0000

    tasks 5, 6, 7

--- tests/more_functions_nested_loops/5-more_numbers.c
+++ tests/more_functions_nested_loops/5-more_numbers.c
@@ -5,11 +5,20 @@ void more_numbers(void)
 	int row;
 	int column;
 
-	for (row = 0; row <=14; row++)
-		for (column = 0; column <= 10; column++)
+	for (row = 0; row <=10; row++)
+	{
+		for (column = 0; column < 15; column++)
 		{
-			_putchar(row);
-			_putchar(column);
+			if (column > 9)
+			{
+			_putchar('0' + column / 10);
+			_putchar('0' + column % 10);
+			}
+			else
+			{
+				_putchar('0' + column);
+			}
 		}
-	_putchar('\n');
+		_putchar('\n');
+	}
 }
