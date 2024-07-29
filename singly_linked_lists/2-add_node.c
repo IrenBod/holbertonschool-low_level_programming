db6a7b4bf2034f543ac03296be7e541a7c5f2ad4
IrenBod <>
2024-07-29 11:42:50 +0000

    task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,9 +11,8 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
+	list_t *new_node = malloc(sizeof(list_t));
 
-	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
@@ -21,7 +20,6 @@ list_t *add_node(list_t **head, const char *str)
 	{free(new_node);
 		return (NULL);
 	}
-	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
