190a82617db1fdd0cea35b8449e1ef0fb4125d78
IrenBod <>
2024-07-31 16:46:27 +0000

    change task 2 len

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -5,7 +5,7 @@
 
 /**
  * print_list - Prints all the elements of a list_t list.
- * @h: Pointer to the head of the list_t list.
+ * @h: Pointer to the head of the list_t list:wqi.
  * Description:
  * This function traverses a linked list of type list_t,
  * printing each element's length and string. If the string
--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -26,6 +26,8 @@ list_t *add_node(list_t **head, const char *str)
 			free(newNode);
 			return (NULL);
 		}
+
+		newNode->len = strlen(str);
 		newNode->next = *head;
 		*head = newNode;
 		return (newNode);
